9549d58553874f70baf701ebd5706a6e7d68cd15
Senthuran100 <senthuran.manoharan@gmail.com>
2021-09-05 20:00:25 +0530

    Add right Shift Array method

---
+++ src/com/senthuran/IsPalindrome.java
@@ -0,0 +1,34 @@
+package com.senthuran;
+
+public class IsPalindrome {
+
+    public boolean checkPalindrom(String word) {
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) == word.charAt(word.length() - i - 1)) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void rightShiftArray(int[] arr, int n){
+        int[] output=new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            int newLocation = (i+(arr.length-1-n))%arr.length;
+            output[newLocation] = arr[i];
+        }
+        for (int i = 0; i < output.length; i++) {
+            System.out.print(output[i]+",");
+        }
+    }
+
+    public static void main(String[] args) {
+        IsPalindrome isPalindrome = new IsPalindrome();
+        String testWord = "blaalb";
+        System.out.println(testWord + " is " + (isPalindrome.checkPalindrom(testWord) ? " a palindrome" : " not a palindrome"));
+        int[] myIntArray = new int[]{1, 2, 3,4,5};
+        isPalindrome.rightShiftArray(myIntArray,2);
+    }
+}
