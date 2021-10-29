70906f61334e5dfe4c9efc5b3eac8bd53f471ae8
Senthuran100 <senthuran.manoharan@gmail.com>
2021-10-29 12:44:36 +0530

    Add Anagram implementation

---
+++ src/com/senthuran/isAnagram.java
@@ -0,0 +1,35 @@
+package com.senthuran;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class isAnagram {
+
+    public static boolean isAnagram(String A, String B) {
+        if (A == null || B == null) {
+            return false;
+        }
+        if (A.length() != B.length()) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : A.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : B.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return false;
+            } else {
+                map.put(c, map.get(c) - 1);
+                if (map.get(c) == 0) {
+                    map.remove(c);
+                }
+            }
+        }
+        return map.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("tommarvoloriddle", "iamlordvoldemort"));
+    }
+}
