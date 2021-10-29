845ca686ed2a47c04a29559728eb7fa849768912
Senthuran100 <senthuran.manoharan@gmail.com>
2021-10-29 09:41:50 +0530

    Add findMaxLenSubarray

--- src/com/senthuran/DuplicateArray.java
+++ src/com/senthuran/DuplicateArray.java
@@ -6,7 +6,7 @@ public class DuplicateArray {
 
     public static List<Integer> findDuplicate(int[] array) {
         Map<Integer, Integer> elements = new HashMap<>();
-        List<Integer> newArray=new ArrayList<>();
+        List<Integer> newArray = new ArrayList<>();
         for (int i : array) {
             if (elements.containsKey(i)) {
                 elements.put(i, elements.get(i) + 1);
@@ -32,9 +32,46 @@ public class DuplicateArray {
         return newArray;
     }
 
+    public static void findMaxLenSubarray(int[] arr, int S) {
+        int target = 0;
+        int length = 0;
+        int endIndex = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            target = 0;
+            for (int j = i; j < arr.length; j++) {
+                target += arr[j];
+                if (S == target) {
+                    if (length < j - i + 1) {
+                        length = j - i + 1;
+                        endIndex = j;
+                    }
+                }
+            }
+
+        }
+        System.out.println("Maximum length sub array:" + length + "  [" + (endIndex - length + 1) + "," + endIndex + "]");
+    }
+
+    public static int maxProduct(int num[]) {
+        int product = 0;
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i+1; j < num.length; j++) {
+                if (num[i] * num[j] > product) {
+                    product = num[i] * num[j];
+                }
+            }
+        }
+        return product;
+    }
+
     public static void main(String[] args) {
         int[] array = {23, 63, 34, 23, 89, 4, 45, 93, 63};
 
+        int[] nums = {5, 6, -5, 5, 3, 5, 3, -2, 0};
+        int target = 8;
+
         findDuplicate(array).stream().forEach(System.out::println);
+        findMaxLenSubarray(nums, target);
+        System.out.println("Maximum product " + maxProduct(nums));
     }
 }
