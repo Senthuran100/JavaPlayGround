b922783b3955d605e215605ee307874daa5dfedc
Senthuran100 <senthuran.manoharan@gmail.com>
2021-10-30 20:16:12 +0530

    Add reArrange implementation

---
+++ src/com/senthuran/alternateHighLow.java
@@ -0,0 +1,29 @@
+package com.senthuran;
+
+import java.util.Arrays;
+
+public class alternateHighLow {
+
+    public static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
+    public static void reArrange(int[] arr) {
+        for (int i = 1; i < arr.length; i = i + 2) {
+            if (arr[i - 1] > arr[i]) {
+                swap(arr, i - 1, i);
+            }
+            if (i + 1 < arr.length && arr[i + 1] > arr[i]) {
+                swap(arr, i + 1, i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 6, 10, 3, 7};
+        reArrange(arr);
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}
