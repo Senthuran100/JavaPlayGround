a2cc5e804842633996bfb0e66bddca1ee348b9f3
Senthuran100 <senthuran.manoharan@gmail.com>
2021-12-11 18:46:14 +0530

    Add RefString Count Program

---
+++ src/com/senthuran/RefStringCount.java
@@ -0,0 +1,20 @@
+package com.senthuran;
+
+import java.util.HashMap;
+
+public class RefStringCount {
+
+    public static void countCharcters(String refString, String InputString) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : InputString.toLowerCase().toCharArray()) {
+            long count = refString.toLowerCase().chars().filter(x -> x == c).count();
+            System.out.println("Character " + c + " count " + count);
+        }
+    }
+
+    public static void main(String[] args) {
+        String input = "Hello World Please";
+        String refString = "Help";
+        countCharcters(input, refString);
+    }
+}
