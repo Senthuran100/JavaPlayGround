bee1b1e85db1e7837dc35592c68fd3bf6669c4c4
Senthuran100 <senthuran.manoharan@gmail.com>
2022-10-16 17:28:51 +0530

    Add Comment largestDistinctSubArray problem.

--- src/com/senthuran/largestDistinctSubArray.java
+++ src/com/senthuran/largestDistinctSubArray.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Find the largest distinct elements in an array.
+ */
 public class largestDistinctSubArray {
 
     public static int[] largestSubArray(int[] arr) {
