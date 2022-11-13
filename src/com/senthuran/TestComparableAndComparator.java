1ceba714bc0ecacdc8aba552a418a7350567d16e
Senthuran100 <senthuran.manoharan@gmail.com>
2022-11-13 09:05:32 +0800

    Add TestComparableAndComparator class

--- src/com/senthuran/TestComparableAndComparator.java
+++ src/com/senthuran/TestComparableAndComparator.java
@@ -2,6 +2,7 @@ package com.senthuran;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 class Movie implements Comparable<Movie> {
 
@@ -33,20 +34,57 @@ class Movie implements Comparable<Movie> {
     }
 }
 
+class MovieNameComparator implements Comparator<Movie> {
+
+    @Override
+    public int compare(Movie movie, Movie t1) {
+        return movie.getName().compareTo(t1.getName());
+    }
+}
+
+class MovieRatingComparator implements Comparator<Movie> {
+
+    @Override
+    public int compare(Movie movie, Movie t1) {
+        if (movie.getRating() > t1.getRating())
+            return 1;
+        else if (movie.getRating() < t1.getRating())
+            return -1;
+        else
+            return 0;
+    }
+}
+
 public class TestComparableAndComparator {
 
 
     public static void main(String[] args) {
         ArrayList<Movie> movies = new ArrayList<>();
         movies.add(new Movie(6.0, "ABC", 2000));
-        movies.add(new Movie(7.0, "CDE", 2001));
-        movies.add(new Movie(8.0, "XYZ", 1995));
+        movies.add(new Movie(7.0, "XYZ", 2001));
+        movies.add(new Movie(8.0, "CDE", 1995));
 
         Collections.sort(movies);
 
-        System.out.println("Sorted Movie List");
-        for (Movie m :
-                movies) {
+        // Default Sorting based on Year.
+        System.out.println("Sorted Movies List based on Year");
+        for (Movie m : movies) {
+            System.out.println("Movie : " + m.getName() + "  " + m.getYear() + "  " + m.getRating());
+        }
+
+        // Sorting based on the Movie Name.
+        MovieNameComparator movieNameComparator = new MovieNameComparator();
+        Collections.sort(movies, movieNameComparator);
+        System.out.println("Sorted Movies List based on Name");
+        for (Movie m : movies) {
+            System.out.println("Movie : " + m.getName() + "  " + m.getYear() + "  " + m.getRating());
+        }
+
+        // Sorting based on the Movie Rating.
+        MovieRatingComparator movieRatingComparator =  new MovieRatingComparator();
+        Collections.sort(movies,movieRatingComparator);
+        System.out.println("Sorted Movies List based on Rating");
+        for (Movie m : movies) {
             System.out.println("Movie : " + m.getName() + "  " + m.getYear() + "  " + m.getRating());
         }
     }
